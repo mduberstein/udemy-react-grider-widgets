7c64f8838b0766085a1c40b748305cf02deccf87
Michael Duberstein <dubermic@flutterint.com>
2022-07-24 16:10:06 -0400

    Widgets: After Clip 140 Introducing useState

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,65 +1,74 @@
-import React from "react";
+import React, {useState} from "react";
 
-// Class Based
-class Accordion extends React.Component {
-// Point 1: Initialize State
-  state = {activeIndex: 0}
+// Class Component
+// class Accordion extends React.Component {
+// // Point 1: Initialize State
+//   state = {activeIndex: 0}
 
-  onTitleClick = (index) => {
-    console.log('Title clicked', index);
-    // Point 2: Update State using this.setState
-    this.setState({activeIndex: index});
-  }
-
-  render () {
-    const items = this.props.items;
-    const renderedItems = items.map((item, index) => {
-      return (
-        <React.Fragment key={item.title}>
-          <div className="title active" 
-           onClick={()=>this.onTitleClick(index)}>
-            <i className="dropdown icon"></i>
-            {item.title}
-          </div>
-          <div className="content active">
-            <p>{item.content}</p>
-          </div>
-        </ React.Fragment>
-      );
-    });
-  
-    return (
-      <div className="ui container">
-        <div className="ui styled accordion">{renderedItems}</div>
-        {/* Point 3: Reference state using this.state at any point in the Application */}
-        <div>{`Active Item Index: ${this.state.activeIndex}`}</div>       
-      </div>
-    );
-  }
-}
-
-// Functional
-// const Accordion = ({items}) => {
-//   const onTitleClick = (index) => {
+//   onTitleClick = (index) => {
 //     console.log('Title clicked', index);
-//   } 
+//     // Point 2: Update State using this.setState
+//     this.setState({activeIndex: index});
+//   }
 
-//   const renderedItems = items.map((item, index) => {
+//   render () {
+//     const items = this.props.items;
+//     const renderedItems = items.map((item, index) => {
+//       return (
+//         <React.Fragment key={item.title}>
+//           <div className="title active" 
+//            onClick={()=>this.onTitleClick(index)}>
+//             <i className="dropdown icon"></i>
+//             {item.title}
+//           </div>
+//           <div className="content active">
+//             <p>{item.content}</p>
+//           </div>
+//         </ React.Fragment>
+//       );
+//     });
+  
 //     return (
-//       <React.Fragment key={item.title}>
-//         <div className="title active" 
-//          onClick={()=> onTitleClick(index)}>
-//           <i className="dropdown icon"></i>
-//           {item.title}
-//         </div>
-//         <div className="content active">
-//           <p>{item.content}</p>
-//         </div>
-//       </ React.Fragment>
+//       <div className="ui container">
+//         <div className="ui styled accordion">{renderedItems}</div>
+//         {/* Point 3: Reference state using this.state at any point in the Application */}
+//         <div>{`Active Item Index: ${this.state.activeIndex}`}</div>       
+//       </div>
 //     );
-//   });
+//   }
+// }
 
-//   return <div className="ui styled accordion">{renderedItems}</div>
-// };
+// Function Component
+const Accordion = ({items}) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+    // console.log('Title clicked', index);
+  } 
+
+  const renderedItems = items.map((item, index) => {
+    return (
+      <React.Fragment key={item.title}>
+        <div className="title active" 
+         onClick={()=> onTitleClick(index)}>
+          <i className="dropdown icon"></i>
+          {item.title}
+        </div>
+        <div className="content active">
+          <p>{item.content}</p>
+        </div>
+      </ React.Fragment>
+    );
+  });
+
+  return  <div className="ui styled accordion">
+            {renderedItems}
+            {/* null value in ${null} displays string 'null' */}
+            <h1>{`Active Item Index: ${activeIndex}`}</h1>
+            {/* null value in {} produces no display in HTML */}
+           {/* <h1>{activeIndex}</h1>   */}
+          </div>;
+};
 
 export default Accordion;
