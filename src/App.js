ee3edde39d1a3acf8043ea740adc889c5eb07041
Michael Duberstein <dubermic@flutterint.com>
2022-07-24 17:07:18 -0400

    Widgets: After Clip 148 Scaffolding the Widget, Search

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './components/Accordion';
+import Search from './components/Search';
 
 const items = [
   {
@@ -19,8 +20,8 @@ const items = [
 const App =  () => {
   return (
     <div>
-      <br />
-      <Accordion items={items} />
+      {/* <Accordion items={items} /> */}
+      <Search />
     </div>
     );
 };
