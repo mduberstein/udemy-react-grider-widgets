f0e769f0094880d7522e5316771c1b4cbc1f9ef2
Michael Duberstein <dubermic@flutterint.com>
2022-07-24 15:35:11 -0400

    Widgets: In Clip 140 Introducing useState, after coding state into Class Based option of Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,55 +1,65 @@
 import React from "react";
 
 // Class Based
-// class Accordion extends React.Component {
-// onTitleClick = (index) => {
-//   console.log('Title clicked', index);
-// }
+class Accordion extends React.Component {
+// Point 1: Initialize State
+  state = {activeIndex: 0}
 
-//   render () {
-//     const items = this.props.items;
-//     const renderedItems = items.map((item, index) => {
-//       return (
-//         <React.Fragment key={item.title}>
-//           <div className="title active" 
-//            onClick={()=>this.onTitleClick(index)}>
-//             <i className="dropdown icon"></i>
-//             {item.title}
-//           </div>
-//           <div className="content active">
-//             <p>{item.content}</p>
-//           </div>
-//         </ React.Fragment>
-//       );
-//     });
+  onTitleClick = (index) => {
+    console.log('Title clicked', index);
+    // Point 2: Update State using this.setState
+    this.setState({activeIndex: index});
+  }
+
+  render () {
+    const items = this.props.items;
+    const renderedItems = items.map((item, index) => {
+      return (
+        <React.Fragment key={item.title}>
+          <div className="title active" 
+           onClick={()=>this.onTitleClick(index)}>
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className="content active">
+            <p>{item.content}</p>
+          </div>
+        </ React.Fragment>
+      );
+    });
   
-//     return <div className="ui styled accordion">{renderedItems}</div>;
-//   }
-// }
+    return (
+      <div className="ui container">
+        <div className="ui styled accordion">{renderedItems}</div>
+        {/* Point 3: Reference state using this.state at any point in the Application */}
+        <div>{`Active Item Index: ${this.state.activeIndex}`}</div>       
+      </div>
+    );
+  }
+}
 
 // Functional
-const Accordion = ({items}) => {
-  const onTitleClick = (index) => {
-    console.log('Title clicked', index);
-  } 
+// const Accordion = ({items}) => {
+//   const onTitleClick = (index) => {
+//     console.log('Title clicked', index);
+//   } 
 
-  const renderedItems = items.map((item, index) => {
-    return (
-      <React.Fragment key={item.title}>
-        <div className="title active" 
-         onClick={()=> onTitleClick(index)}>
-          <i className="dropdown icon"></i>
-          {item.title}
-        </div>
-        <div className="content active">
-          <p>{item.content}</p>
-        </div>
-      </ React.Fragment>
-    );
-  });
+//   const renderedItems = items.map((item, index) => {
+//     return (
+//       <React.Fragment key={item.title}>
+//         <div className="title active" 
+//          onClick={()=> onTitleClick(index)}>
+//           <i className="dropdown icon"></i>
+//           {item.title}
+//         </div>
+//         <div className="content active">
+//           <p>{item.content}</p>
+//         </div>
+//       </ React.Fragment>
+//     );
+//   });
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
-
-};
+//   return <div className="ui styled accordion">{renderedItems}</div>
+// };
 
 export default Accordion;
