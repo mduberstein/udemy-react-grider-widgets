9f2c062b23ee3612f462da8dfd875f3e3df31362
Michael Duberstein <dubermic@flutterint.com>
2022-07-24 14:04:06 -0400

    Widgets: In Clip 139 Helper Functions in Function Components, Class Based option of Accordion

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,22 +1,49 @@
 import React from "react";
 
-const Accordion = ({items}) => {
-  const renderedItems = items.map(item => {
-    return (
-      <React.Fragment key={item.title}>
-        <div className="title active">
-          <i className="dropdown icon"></i>
-          {item.title}
-        </div>
-        <div className="content active">
-          <p>{item.content}</p>
-        </div>
-      </ React.Fragment>
-    );
-  });
+class Accordion extends React.Component {
+onTitleClick = (index) => {
+  console.log('Title clicked', index);
+}
 
-  return <div className="ui styled accordion">{renderedItems}</div>;
+  render () {
+    const items = this.props.items;
+    const renderedItems = items.map((item, index) => {
+      return (
+        <React.Fragment key={item.title}>
+          <div className="title active" 
+           onClick={()=>this.onTitleClick(index)}>
+            <i className="dropdown icon"></i>
+            {item.title}
+          </div>
+          <div className="content active">
+            <p>{item.content}</p>
+          </div>
+        </ React.Fragment>
+      );
+    });
+  
+    return <div className="ui styled accordion">{renderedItems}</div>;
+  }
+}
 
-};
+// const Accordion = ({items}) => {
+//   const renderedItems = items.map((item, index) => {
+//     return (
+//       <React.Fragment key={item.title}>
+//         <div className="title active" 
+//          onClick={()=>console.log('Title clicked', index)}>
+//           <i className="dropdown icon"></i>
+//           {item.title}
+//         </div>
+//         <div className="content active">
+//           <p>{item.content}</p>
+//         </div>
+//       </ React.Fragment>
+//     );
+//   });
+
+//   return <div className="ui styled accordion">{renderedItems}</div>;
+
+// };
 
 export default Accordion;
