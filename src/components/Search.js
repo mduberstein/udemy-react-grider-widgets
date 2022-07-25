1ee418ab0a74eec175d2d0be183d6a8f6c07d813
Michael Duberstein <dubermic@flutterint.com>
2022-07-25 14:10:11 -0400

    After Clip 154 Executing the Request from useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,5 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
 const Search = () => {
   const[term, setTerm] = useState('');
@@ -12,9 +13,41 @@ const Search = () => {
   //   console.log('I RUN WITH EVERY RENDER AND AT INITIAL RENDER');
   // });
 
-  useEffect(()=>{
-    console.log('I RUN AT INITIAL RENDER AND, WITH EVERY RENDER IF TERM CHANGES)');
+  // useEffect(()=>{
+  //   console.log('I RUN AT INITIAL RENDER AND, WITH EVERY RENDER IF TERM CHANGES)');
+  // }, [term]);
+
+  // Async ALT 1
+  useEffect(() => {
+    const search = async () => {
+//https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=programming
+      await axios.get('https://en.wikipedia.org/w/api.php', {
+        params: {
+          action: 'query',
+          list: 'search',
+          origin: '*',
+          format: 'json',
+          srsearch: term
+        }
+      });
+    };
+    search();
   }, [term]);
+
+  // Async ALT 2, IIFE, no performance benefit over ALT 1
+  // useEffect(() => {
+  //   (async () => {
+  //     await axios.get("some url");
+  //   })();
+  // }, [term]);
+
+  // Async ALT 3, Normal promises
+  // useEffect(() => {
+  //   axios.get("some url")
+  //     .then(response => {
+  //       console.log(response.data);
+  //     });
+  // }, [term]);
 
   return (
     <div className="ui form">
