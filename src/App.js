d4979df33b99808ef3f57dc5912684cbabfe8131
Michael Duberstein <dubermic@flutterint.com>
2022-07-26 11:00:21 -0400

    After Clip 171 Selection State

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Dropdown from './components/Dropdown';
-import Search from './components/Search';
+import React, { useState } from "react";
+import Accordion from "./components/Accordion";
+import Dropdown from "./components/Dropdown";
+import Search from "./components/Search";
 
 const items = [
   {
-    title: 'What is React?',
-    content: 'React is a front end framework'
+    title: "What is React?",
+    content: "React is a front end framework",
   },
   {
-    title: 'Why use React?',
-    content: 'React is a favorit JS library among engineers'
+    title: "Why use React?",
+    content: "React is a favorit JS library among engineers",
   },
   {
-    title: 'How do you use React?',
-    content: 'You use React by creating components'
+    title: "How do you use React?",
+    content: "You use React by creating components",
   },
-]
+];
 
 const options = [
   {
-    label: 'The  Color Red',
-    value: 'red'
+    label: "The  Color Red",
+    value: "red",
   },
   {
-    label: 'The  Color Green',
-    value: 'green'
+    label: "The  Color Green",
+    value: "green",
   },
   {
-    label: 'A Shade of Blue',
-    value: 'blue'
-  }
+    label: "A Shade of Blue",
+    value: "blue",
+  },
 ];
 
-const App =  () => {
+const App = () => {
+  const [selected, setSelected] = useState(options[0]);
   return (
     <div>
       {/* <Accordion items={items} /> */}
       {/* <Search /> */}
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
-    );
+  );
 };
 
 export default App;
