bf98e4f7f33ad827592b49230d0eabf72bfafb99
Michael Duberstein <dubermic@flutterint.com>
2022-07-26 19:40:11 -0400

    After Clip 179 Why Stay Open?, console logs

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,18 +1,31 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false); // state of dropdown
+  useEffect(() => {
+    document.body.addEventListener(
+      "click",
+      () => {
+        console.log("In body click handler, open =", open);
+        setOpen(false);
+      }
+      , { capture: true }
+    );
+  }, []);
 
   const renderedOptions = options.map((option) => {
-    if(option.value === selected.value) {
+    if (option.value === selected.value) {
       // null in react means "do not render anything"
-      return null; 
-    } 
+      return null;
+    }
     return (
-      <div 
-        key={option.value} 
+      <div
+        key={option.value}
         className="item"
-        onClick={() => onSelectedChange(option)}
+        onClick={() => {
+          console.log("In Item click handler, open =", open);
+          onSelectedChange(option);
+        }}
       >
         {option.label}
       </div>
@@ -22,11 +35,19 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active': ''}`}>
+        <div
+          onClick={() => {
+            setOpen(!open);
+            console.log("In Dropdown div click handler, open =", open);
+          }}
+          className={`ui selection dropdown ${open ? "visible active" : ""}`}
+        >
           {/* i is a convention Semantic UI uses to display icons, in Html spec i is for italics */}
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className={`menu ${open ? 'visible transition': ''}`}>{renderedOptions}</div>
+          <div className={`menu ${open ? "visible transition" : ""}`}>
+            {renderedOptions}
+          </div>
         </div>
       </div>
     </div>
