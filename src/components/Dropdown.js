83f14e72c05a78a888753667b507d3548223700d
Michael Duberstein <dubermic@flutterint.com>
2022-07-26 11:42:29 -0400

    After Clip 173 Hiding and Showing the Option List

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false); // state of dropdown
+
   const renderedOptions = options.map((option) => {
+    if(option.value === selected.value) {
+      // null in react means "do not render anything"
+      return null; 
+    } 
     return (
       <div 
         key={option.value} 
@@ -16,11 +22,11 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
     <div className="ui form">
       <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div onClick={()=> setOpen(!open)} className={`ui selection dropdown ${open ? 'visible active': ''}`}>
           {/* i is a convention Semantic UI uses to display icons, in Html spec i is for italics */}
           <i className="dropdown icon"></i>
           <div className="text">{selected.label}</div>
-          <div className="menu visible transition">{renderedOptions}</div>
+          <div className={`menu ${open ? 'visible transition': ''}`}>{renderedOptions}</div>
         </div>
       </div>
     </div>
