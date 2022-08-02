d632b7b26dd0340a0650ded9ab563a62d52f5001
Michael Duberstein <dubermic@flutterint.com>
2022-08-02 07:06:09 -0400

    After Clip 190 Using the Google Translate API

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,8 +1,16 @@
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 const Convert = ({language, text}) => {
   useEffect(() => {
-    console.log('In Convert, New language or text')
+    // console.log('In Convert, New language or text');
+    axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+      params: {
+        q: text,
+        target: language.value,
+        key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+      }
+    });
   }, [language, text]);
   return <div></div>;
 }
