3a610c593995fde2c5d922075a185695d25e8792
Michael Duberstein <dubermic@flutterint.com>
2022-08-07 16:32:26 -0400

    After Clip 203 Updating the Route

--- src/components/Route.js
+++ src/components/Route.js
@@ -1,9 +1,11 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 
 const Route = ({path, children}) => {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
   useEffect(() => {
     const onLocationChange = () => {
-      console.log('Location Change to ', window.location.href);
+      // console.log('Location Change to ', window.location.href);
+      setCurrentPath(window.location.pathname);
     }
     window.addEventListener('popstate', onLocationChange);
     return () => {
@@ -11,7 +13,7 @@ const Route = ({path, children}) => {
     };
   },[]);
 
-  return window.location.pathname === path
+  return currentPath === path
     ? children
     : null;
 };
