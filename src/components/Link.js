63a5742a334a1086ca6c5ad58e8f662e1a0110f2
Michael Duberstein <dubermic@flutterint.com>
2022-08-07 16:11:11 -0400

    After Clip 201 Clip 202 Detecting Navigation

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,6 +5,9 @@ const Link = ({className, href, children}) => {
     // prevent default browser behavior of full page reload
     event.preventDefault();
     window.history.pushState({}, '', href);
+
+    const navEvent = new PopStateEvent('popstate');
+    window.dispatchEvent(navEvent);
   };
 
   return (
--- src/components/Route.js
+++ src/components/Route.js
@@ -1,5 +1,16 @@
+import {useEffect} from 'react';
 
 const Route = ({path, children}) => {
+  useEffect(() => {
+    const onLocationChange = () => {
+      console.log('Location Change to ', window.location.href);
+    }
+    window.addEventListener('popstate', onLocationChange);
+    return () => {
+      window.removeEventListener('popstate', onLocationChange);
+    };
+  },[]);
+
   return window.location.pathname === path
     ? children
     : null;
